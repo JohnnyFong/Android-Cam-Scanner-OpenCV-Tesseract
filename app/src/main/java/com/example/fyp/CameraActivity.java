bd1a407373ad5641df981e4319151c072d60183d
JohnnyFong <johnnyfongsx@gmail.com>
2019-03-13 00:13:36 +0800

    Canny Edge Detection to find largest rectangle done

--- app/src/main/java/com/example/fyp/CameraActivity.java
+++ app/src/main/java/com/example/fyp/CameraActivity.java
@@ -20,12 +20,20 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -40,7 +48,10 @@ public class CameraActivity extends AppCompatActivity {
 
     ImageView photo = null;
     Mat imageGray = null;
-    Mat imageCny = null;
+    Mat imageBilateral = null;
+    Mat imageThreshold = null;
+    Mat imageBlur = null;
+    Mat imageBorder = null;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -97,12 +108,6 @@ public class CameraActivity extends AppCompatActivity {
         contButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                new Thread(new Runnable(){
-//                    @Override
-//                    public void run() {
-//                        openCV();
-//                    }
-//                }).start();
                 openCV();
 
             }
@@ -118,38 +123,79 @@ public class CameraActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, 1);
         }
         imageGray = new Mat();
-        imageCny = new Mat();
+        imageBilateral = new Mat();
+        imageThreshold = new Mat();
+        imageBlur = new Mat();
+        imageBorder = new Mat();
+        Mat imageSource = Imgcodecs.imread(currentPhotoPath);
 
         //Toast.makeText(this,"Opencv starting",Toast.LENGTH_LONG).show();
-        Imgproc.cvtColor(Imgcodecs.imread(currentPhotoPath),imageGray, Imgproc.COLOR_BGR2RGB);
-        Imgproc.Canny(imageGray,imageCny,10,100,3,true);
-        File photoFile = null;
-        try{
-            photoFile = createImageFile();
-        }catch(Exception ex){
-            Toast.makeText(this,ex.toString(), Toast.LENGTH_LONG).show();
+        Imgproc.cvtColor(imageSource,imageGray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.bilateralFilter(imageGray,imageBilateral,9,75,75);
+        Imgproc.adaptiveThreshold(imageBilateral,imageThreshold,255,Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C,Imgproc.THRESH_BINARY,115,4);
+        Imgproc.medianBlur(imageThreshold,imageBlur,11);
+        Core.copyMakeBorder(imageBlur,imageBorder,5,5,5,5,Core.BORDER_CONSTANT);
+        Imgproc.Canny(imageBorder,imageSource,10, 100, 3, true);
+        Imgproc.GaussianBlur(imageSource,imageSource,new Size(5,5),5);
+
+        //find the contours
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Imgproc.findContours(imageSource, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        double maxArea = -1;
+        int maxAreaIdx = -1;
+        MatOfPoint temp_contour = contours.get(0); //the largest is at the index 0 for starting point
+        MatOfPoint2f approxCurve = new MatOfPoint2f();
+        Mat largest_contour = contours.get(0);
+        List<MatOfPoint> largest_contours = new ArrayList<MatOfPoint>();
+        for (int idx = 0; idx < contours.size(); idx++) {
+            temp_contour = contours.get(idx);
+            double contourarea = Imgproc.contourArea(temp_contour);
+            //compare this contour to the previous largest contour found
+            if (contourarea > maxArea) {
+                //check if this contour is a square
+                MatOfPoint2f new_mat = new MatOfPoint2f( temp_contour.toArray() );
+                int contourSize = (int)temp_contour.total();
+                Imgproc.approxPolyDP(new_mat, approxCurve, contourSize*0.05, true);
+                if (approxCurve.total() == 4) {
+                    maxArea = contourarea;
+                    maxAreaIdx = idx;
+                    largest_contours.add(temp_contour);
+                    largest_contour = temp_contour;
+                }
+            }
         }
-        if(photoFile != null){
-            Uri photoURI = FileProvider.getUriForFile(this,
-                    "com.example.android.fileprovider",
-                    photoFile);
-            Boolean a= Imgcodecs.imwrite(photoURI.toString(),imageCny);
-            Log.d("imwrite",imageCny.toString() + photoURI.toString() + a);
-        }else{
-            Toast.makeText(this,"failed", Toast.LENGTH_LONG).show();
-        }
-        Bitmap bm = Bitmap.createBitmap(imageCny.cols(), imageCny.rows(),Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(imageCny, bm);
+        MatOfPoint temp_largest = largest_contours.get(largest_contours.size()-1);
+        largest_contours = new ArrayList<MatOfPoint>();
+        largest_contours.add(temp_largest);
+
+        Imgproc.cvtColor(imageSource, imageSource, Imgproc.COLOR_BayerBG2RGB);
+        Imgproc.drawContours(imageSource, largest_contours, -1, new Scalar(0, 255, 0), 1);
+
+
+
+//        List<MatOfPoint> contours = new ArrayList<>();
+//        Mat hierarchy = new Mat();
+//        Imgproc.findContours(imageSource, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+//        Mat drawing = Mat.zeros(imageSource.size(), CvType.CV_8UC3);
+//
+//
+//        for (int i = 0; i < contours.size(); i++) {
+//            Scalar color = new Scalar(rng.nextInt(256), rng.nextInt(256), rng.nextInt(256));
+//            Imgproc.drawContours(drawing, contours, i, color, 2, Core.LINE_8, hierarchy, 0, new Point());
+//        }
+
+        Bitmap bm = Bitmap.createBitmap(imageSource.cols(), imageSource.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(imageSource, bm);
+
 
         try{
-            //Bitmap imageBitmap =  MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.fromFile(new File(currentPhotoPath)));
-
             photo.setImageBitmap(bm);
         }catch(Exception ex){
             //Toast.makeText(this,ex.toString(), Toast.LENGTH_LONG).show();
             Log.d("bitmap",ex.toString());
         }
-        //galleryAddPic();
+
 
     }
 
